d055171797c8ef423117aaa374cd7c39c5233737
31 <85961838+Roman-Trebis@users.noreply.github.com>
2025-06-22 21:20:51 +0300

    Init Commit
    Коммит

---
+++ ConsoleApp.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            #region Start
+            /*
+            Тестовый Проект и Тестовый Код на C#
+            */
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+            #endregion
+            
+        }
+    }
+}
